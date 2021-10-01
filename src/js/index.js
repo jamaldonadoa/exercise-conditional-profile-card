1d180735ceb5f606b986023db21a9a6f090df9ea
jamaldonadoa <jadolfomaldonadoa@gmail.com>
2021-10-01 23:58:33 +0000

    sitio completo

--- src/js/index.js
+++ src/js/index.js
@@ -28,19 +28,41 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-
+  let name = "<h1>Lucy Boilett</h1>";
+  if (variables.name != null && variables.lastname != null)
+    name = `<h1>${variables.name} ${variables.lastname}</h1>`;
+  let role = "<h2>Web Developer</h2>";
+  if (variables.role != null) role = `<h2>${variables.role}</h2>`;
+  let direction = "<h3>Miami, USA</h3>";
+  if (variables.city != null && variables.country != null)
+    direction = `<h3>${variables.city}, ${variables.country}</h3>`;
+  let position = `<ul class="position-left">`;
+  if (variables.socialMediaPosition == "position-right")
+    position = `<ul class="position-right">`;
+  let twitter = `<li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>`;
+  if (variables.twitter != null)
+    twitter = `<li><a href="https://twitter.com/${variables.twitter}"><i class="fa fa-twitter"></i></a></li>`;
+  let github = `<li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>`;
+  if (variables.github != null)
+    github = `<li><a href="https://github.com/${variables.github}"><i class="fa fa-github"></i></a></li>`;
+  let linkedin = `<li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>`;
+  if (variables.linkedin != null)
+    linkedin = `<li><a href="https://linkedin.com/${variables.linkedin}"><i class="fa fa-linkedin"></i></a></li>`;
+  let instagram = `<li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>`;
+  if (variables.instagram != null)
+    instagram = `<li><a href="https://instagram.com/${variables.instagram}"><i class="fa fa-instagram"></i></a></li>`;
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          ${name}
+          ${role}
+          ${direction}
+          ${position}
+            ${twitter}
+            ${github}
+            ${linkedin}
+            ${instagram}
           </ul>
         </div>
     `;
